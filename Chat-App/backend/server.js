1756a78e505a11858bade4b9a9872c6d3c023e58
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2024-01-06 23:46:59 +0530

    authentication

--- Chat-App/backend/server.js
+++ Chat-App/backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const chats = require("./data/data");
 const connectDB = require("./config/db");
-
+const userRoutes = require("./routes/userRoutes")
 
 
 dotenv.config();
@@ -10,15 +10,13 @@ dotenv.config();
 connectDB()
 
 const app = express();
-
+app.use(express.json())  // to accept JSON data
 app.get('/',(req,res)=>[
     res.send("api is running")
 ])
-
-app.get('/api/chats',(req,res)=>{
-    res.send(chats);
-})
-
+app.use('/api/user',userRoutes)
+app.use(notFound);
+app.use(erroHandler);
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT)
