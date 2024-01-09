3a8835597d5ab691542e7ea4bff74776bcacc5dd
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2024-01-09 23:26:09 +0530

    Side Drawer Work

--- Chat-App/frontend/src/index.js
+++ Chat-App/frontend/src/index.js
@@ -1,21 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "./index.css";
 import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import ChatProvider from "./context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <ChakraProvider>
-    <BrowserRouter>
-      <ChatProvider>
-        <App />
-      </ChatProvider>
-    </BrowserRouter>
-  </ChakraProvider>,
-  document.getElementById("root")
+const root = document.getElementById('root');
+const rootContainer = createRoot(root);
+rootContainer.render( 
+<ChakraProvider>
+  <BrowserRouter>
+    <ChatProvider>
+      <App />
+    </ChatProvider>
+  </BrowserRouter>
+</ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
