cc451cabe8d6d0ae579751e010ed5a808fc28b90
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2024-01-14 23:10:09 +0530

    changes

--- Chat-App/frontend/src/components/ChatBox.js
+++ Chat-App/frontend/src/components/ChatBox.js
@@ -19,18 +19,7 @@ const ChatBox = ({fetchagain ,setFetchAgain}) => {
   >
    <SingleChat fetchagain={fetchagain} setFetchAgain={setFetchAgain}/>
   </Box>
-   <Box
-   display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
-   alignItems="center"
-   flexDir="column"
-   p={3}
-   bg="white"
-   w={{ base: "100%", md: "68%" }}
-   borderRadius="lg"
-   borderWidth="1px"
-   >
-    <SingleChat fetchagain={fetchagain} setFetchAgain={setFetchAgain}/>
-   </Box>
+  
   )
 }
 
